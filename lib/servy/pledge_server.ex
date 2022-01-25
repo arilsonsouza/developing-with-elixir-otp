1a272236997e887396d4f0b92802975781104131
Arilson Souza <arilsons61@gmail.com>
2022-01-24 22:55:01 -0300

    28 Stateful Server Processes-3

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -1,32 +1,43 @@
 defmodule Servy.PledgeServer do
+  @name :pledge_server
+  def start do
+    IO.puts("Starting the pledge server...")
+    pid = spawn(__MODULE__, :listen_loop, [[]])
+    Process.register(pid, @name)
+    pid
+  end
+
   def listen_loop(state) do
-    IO.puts("\nWaiting for a message...")
-
     receive do
-      {:create_pledge, name, amount} ->
+      {sender, :create_pledge, name, amount} ->
         {:ok, id} = send_pledge_to_service(name, amount)
-        new_state = [{name, amount} | state]
-        IO.puts("#{name} pledged #{amount}")
-        IO.puts("New state is #{inspect(new_state)}")
-
+        most_recent_pledges = Enum.take(state, 2)
+        new_state = [{name, amount} | most_recent_pledges]
+        send(sender, {:response, id})
         listen_loop(new_state)
 
       {sender, :recent_pledges} ->
         send(sender, {:response, state})
-        IO.puts("Sent pledges to #{inspect(sender)}")
         listen_loop(state)
     end
   end
 
   def create(name, amount) do
-    {:ok, id} = send_pledge_to_service(name, amount)
+    send(@name, {self(), :create_pledge, name, amount})
 
-    # CACHE THE PLEDGE:
-    [{name, amount}]
+    receive do
+      {:response, status} ->
+        status
+    end
   end
 
-  def recent_pledges do
-    # Returns the most recent pledges (cache):
+  def recent_pledges() do
+    send(@name, {self(), :recent_pledges})
+
+    receive do
+      {:response, pledges} ->
+        pledges
+    end
   end
 
   defp send_pledge_to_service(_name, _amount) do
@@ -34,3 +45,14 @@ defmodule Servy.PledgeServer do
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
 end
+
+# alias Servy.PledgeServer
+# PledgeServer.start()
+
+# IO.inspect(PledgeServer.create("larry", 10))
+# IO.inspect(PledgeServer.create("moe", 20))
+# IO.inspect(PledgeServer.create("curly", 30))
+# IO.inspect(PledgeServer.create("daisy", 40))
+# IO.inspect(PledgeServer.create("grace", 50))
+
+# IO.inspect(PledgeServer.recent_pledges())
